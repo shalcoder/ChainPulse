d70b6ef6001694101f5b50d927153cff76117daa
Poornachandran <poorna2006poorna@gmail.com>
2026-04-28 13:38:53 +0530

    ui bug fix

--- frontend/components/layout/Sidebar.tsx
+++ frontend/components/layout/Sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { ThemeToggle } from "@/components/layout/ThemeToggle";
-import { ShortcutHud } from "@/components/ui/ShortcutHud";
+import { ShortcutHud } from "../ui/ShortcutHud";
 
 interface NavItem {
   href: string;
